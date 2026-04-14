ee6040faa02b1527f8886fbdb026d3a12d5ec913
sadbunnygit <danelle06@outlook.com>
2026-04-14 13:41:48 -0400

    added the functionalitly to hte theme button

---
+++ scripts/theme.js
@@ -0,0 +1,24 @@
+
+
+function toggleTheme() 
+{
+    document.body.classList.toggle("dark");
+
+    // remember choice
+    if (document.body.classList.contains("dark")) 
+    {
+        localStorage.setItem("theme", "dark");
+    } 
+    else     
+    {
+        localStorage.setItem("theme", "light");
+    }
+}
+
+window.onload = function () 
+{
+    if (localStorage.getItem("theme") === "dark") 
+    {
+        document.body.classList.add("dark");
+    }
+};
