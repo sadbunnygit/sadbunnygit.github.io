ccb3761dfdb1df8b93a54e697415610539981be1
sadbunnygit <danelle06@outlook.com>
2026-04-14 16:06:44 -0400

    auto loads the projects like skills section does

--- scripts/projects.js
+++ scripts/projects.js
@@ -1,5 +1,29 @@
 
 
+// - not in a function happens on load -
+// get the data 
+fetch('../data/projects.json')
+    .then(r => r.json())
+    .then(renderProjects);
+
+function renderProjects(data) 
+{
+    const root = document.getElementById("projectsContainer");
+
+    data.forEach(project => 
+    {
+        const card = document.createElement("div");
+        card.className = "project-card";
+
+        card.innerHTML = 
+        `
+            <h3>${project.title}</h3>
+            <p>${project.description || ""}</p>
+        `;
+        root.appendChild(card);
+    });
+}
+
 function scrollProjects(direction) 
 {
     const container = document.getElementById("projectsContainer");
