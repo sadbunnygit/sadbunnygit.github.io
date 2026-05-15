d21acbfeb640242d814a8acf6b4256f756d8b4c2
sadbunnygit <danelle06@outlook.com>
2026-05-15 13:33:19 -0400

    hobby section

---
+++ scripts/hobbies.js
@@ -0,0 +1,31 @@
+
+
+// - not in a function happens on load -
+// get the data 
+fetch('../data/hobbies.json')
+    .then(r => r.json())
+    .then(renderHobbies);
+
+// - function -
+// load the html for that data
+function renderHobbies(data) 
+{
+    console.log("Debug: Running renderHobbies");
+    const root = document.getElementById("hobbies");
+    const grid = document.createElement("div"); // make it a grid, so nice alignment
+    grid.className = "skill-grid";
+    data.forEach(hob => 
+    {
+        const card = document.createElement("div"); // create the card for each skill
+        card.className = `skill-card ${hob.level}`; // different style based on level (colour changes)
+
+        card.innerHTML = // write the text
+        `
+            <h3>${hob.name}</h3>
+            <span class="skill-tag ${hob.level}">${hob.level}</span>
+            <p>${hob.description || ""}</p>
+        `;
+
+        grid.appendChild(card);
+    });
+}
