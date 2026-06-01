e6d693dd84adae0b9f26b0f81a618a1583357aa5
sadbunnygit <danelle06@outlook.com>
2026-05-31 22:03:14 -0400

    now the last updated date updates yay

--- scripts/readgithub.js
+++ scripts/readgithub.js
@@ -1,3 +1,5 @@
+
+
 fetch("https://api.github.com/repos/sadbunnygit/sadbunnygit.github.io/branches/main")
 .then(response => 
 {
@@ -7,9 +9,11 @@ fetch("https://api.github.com/repos/sadbunnygit/sadbunnygit.github.io/branches/m
         console.log(json);
         console.log("date (as it in the json): " + json.commit.commit.author.date);
         console.log("date (just date): " + json.commit.commit.author.date.split("T")[0]);
+        document.getElementById("last-updated").textContent = json.commit.commit.author.date.split("T")[0];
     });
 })
 .catch(error => 
 {
     console.log(error);
 });
+
